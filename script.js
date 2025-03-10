6ad32c8722aa426b8829f86614e773205135fc8a
dvesper729 <dvesper@iu.edu>
2025-03-10 19:25:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Change Message Example</title>
-  <script>
-    function changeMessage() {
-      const pElement = document.getElementById('message');
-      pElement.textContent = 'Hello! You clicked the button!';}
-  </script>
-</head>
-<body>
-  <p id="message">Click the button to see a message.</p>
-  <button onclick="changeMessage()">Click Me!</button>
-</body>
-</html>
+const button = document.getElementById('messageButton')
+button.addEventListener('click', function() {
+  const pElement = document.getElementById('message')
+  pElement.textContent = 'Hello! You clicked the button!'
+});
