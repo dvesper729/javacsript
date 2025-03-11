de0d240ffd73d6fdf0c86d21829cc90429afc1d4
dvesper729 <dvesper@iu.edu>
2025-03-10 20:57:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const messageButton = document.getElementById('messageButton');
+const messageButton = document.getElementById('messageButton')
 messageButton.addEventListener('click', function() {
-  const pElement = document.getElementById('message');
-  pElement.textContent = 'Hello! You clicked the button!';
-});
+  const pElement = document.getElementById('message')
+  pElement.textContent = 'Hello! You clicked the button!'
+})
 
-const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
-function changeBackgroundColor() {
-  const bodyElement = document.body;
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  bodyElement.style.backgroundColor = randomColor;
+const colors = ['darkslategrey', 'darkred', 'cadetblue', 'blanchedalmond', 'coral']
+function changebackgroundcolor() {
+  const bodyElement = document.body
+  const randomColor = colors[Math.floor(Math.random() * colors.length)]
+  bodyElement.style.backgroundColor = randomColor
 }
 
-const colorChangeButton = document.getElementById('colorchangebutton');
-colorChangeButton.addEventListener('click', changeBackgroundColor);
+const colorchangebutton = document.getElementById('colorchangebutton')
+colorchangebutton.addEventListener('click', changeBackgroundColor)
+
+function namegreeting() {
+    let name = document.getElementById("nameentry").value
+    let greetingMessage = "Hello, " + name + "!"
+    document.getElementById("greeting").textContent = greetingMessage
+}
