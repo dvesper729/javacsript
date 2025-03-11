8a0d4e68818ef0291f7327448c76bdc4bebab1fb
dvesper729 <dvesper@iu.edu>
2025-03-10 20:15:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const button = document.getElementById('messageButton')
-button.addEventListener('click', function() {
-  const pElement = document.getElementById('message')
-  pElement.textContent = 'Hello! You clicked the button!'})
+const messageButton = document.getElementById('messageButton');
+messageButton.addEventListener('click', function() {
+  const pElement = document.getElementById('message');
+  pElement.textContent = 'Hello! You clicked the button!';
+});
+
 const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
-    function changeBackgroundColor() {
-    const bodyElement = document.body
-    const randomColor = colors[Math.floor(Math.random() * colors.length)]
-      bodyElement.style.backgroundColor = randomColor}
-const button = document.getElementById('colorchangebutton')
-button.addEventListener('click', changeBackgroundColor)
+function changeBackgroundColor() {
+  const bodyElement = document.body;
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  bodyElement.style.backgroundColor = randomColor;
+}
+
+const colorChangeButton = document.getElementById('colorchangebutton');
+colorChangeButton.addEventListener('click', changeBackgroundColor);
