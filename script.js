3289378d54a0e6cad858b2879cde1bbcebd50661
dvesper729 <dvesper@iu.edu>
2025-03-10 21:16:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-const messageButton = document.getElementById('messageButton')
+const messageButton = document.getElementById('messageButton');
 messageButton.addEventListener('click', function() {
-  const pElement = document.getElementById('message')
-  pElement.textContent = 'Hello! You clicked the button!'
-})
+  const pElement = document.getElementById('message');
+  pElement.textContent = 'Hello! You clicked the button!';
+});
 
-const colors = ['darkslategrey', 'darkred', 'cadetblue', 'blanchedalmond', 'coral']
+const colors = ['darkslategrey', 'darkred', 'cadetblue', 'blanchedalmond', 'coral'];
 function changeBackgroundColor() {
-  const bodyElement = document.body
-  const randomColor = colors[Math.floor(Math.random() * colors.length)]
-  bodyElement.style.backgroundColor = randomColor
+  const bodyElement = document.body;
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  bodyElement.style.backgroundColor = randomColor;
 }
 
-const colorchangebutton = document.getElementById('colorchangebutton')
-colorchangebutton.addEventListener('click', changeBackgroundColor)
+const colorchangebutton = document.getElementById('colorchangebutton');
+colorchangebutton.addEventListener('click', changeBackgroundColor);
 
-function namegreeting() {
-    let name = document.getElementById("nameentry").value
-    let greetingMessage = "Hello, " + name + "!"
-    document.getElementById("greeting").textContent = greetingMessage
+function displayGreeting() {
+    let name = document.getElementById("name").value;
+    let greetingMessage = "Hello, " + name + "!";
+    document.getElementById("greeting").textContent = greetingMessage;
 }
-const nameentrybutton = document.getElementById('nameentrybutton')
-nameentrybutton.addEventListener('click', function() {
-  let name = document.getElementById("name").value
-  const pElement = document.getElementById('greeting')
-  pElement.textContent = 'Hello, ' + name + '!'
-}
+
+const nameentrybutton = document.getElementById('nameentrybutton');
+nameentrybutton.addEventListener("click", displayGreeting);
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    displayGreeting();
+});
